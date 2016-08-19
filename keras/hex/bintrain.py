835a8eeb9304b64a077abc3a48c09f950f4365f5
alingse <alingse@foxmail.com>
2016-08-19 16:53:13 +0800

    update:4层训练

--- keras/hex/bintrain.py
+++ keras/hex/bintrain.py
@@ -5,9 +5,9 @@
 
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import Dropout, Activation, Flatten,RepeatVector
-from keras.layers import Embedding
-from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Activation
+from keras.models import model_from_json
 
 from keras.optimizers import SGD, Adadelta, Adagrad,RMSprop
 from keras.utils import np_utils, generic_utils
@@ -15,6 +15,8 @@ from keras.utils import np_utils, generic_utils
 import numpy as np
 from random import shuffle
 from random import choice
+
+import sys
 
 
 #0 --> 255
@@ -26,8 +28,8 @@ def load_XY(binlen = 8):
 
     count = maxnum + 1
 
-    X_train = np.zeros((count,numlen),dtype=np.uint8)
-    Y_train = np.zeros((count,binlen),dtype=np.uint8)
+    X_train = np.zeros((count,numlen),dtype=np.float32)
+    Y_train = np.zeros((count,binlen),dtype=np.float32)
     for i in range(count):
 
         i_str = str(i).zfill(numlen)
@@ -37,16 +39,19 @@ def load_XY(binlen = 8):
 
         X_train[i] = x_seq
         Y_train[i] = y_seq
+    
+    x_test = X_train
+    y_test = Y_train
 
-    X_train = np.repeat(X_train,300,axis=0)
-    Y_train = np.repeat(Y_train,300,axis=0)
+    X_train = np.repeat(X_train,500,axis=0)
+    Y_train = np.repeat(Y_train,500,axis=0)
 
     index = list(range(X_train.shape[0]))
     shuffle(index)
     X_train = X_train[index]
     Y_train = Y_train[index]
     
-    return X_train,Y_train
+    return X_train,Y_train,x_test,y_test
 
 
 def train(X_train,Y_train):
@@ -79,7 +84,7 @@ def train(X_train,Y_train):
 
     print('start fit')
     model.fit(X_train, Y_train,
-              batch_size=100, nb_epoch=3000,
+              batch_size=100, nb_epoch=1500,
               verbose=2,shuffle=True,
               validation_split=0.2)
 
@@ -92,21 +97,33 @@ def dump(model,save_name):
         f.write(model.to_json())
     model.save_weights('{}.model.weigthts.h5'.format(save_name))
 
+def load(name):
+    with open('{}.model.json'.format(name),'r') as f:
+        model = model_from_json(f.read())
+    model.load_weights('{}.model.weigthts.h5'.format(name))
+    return model
+
 
 def main(name='test'):
+
+    binlen = 4
     
-    X_train,Y_train = load_XY(binlen=3)
+    X_train,Y_train,x_test,y_test = load_XY(binlen=binlen)
     
     model = train(X_train,Y_train)
     score = model.evaluate(X_train,Y_train,batch_size=20,verbose=2)
     print(score)
+    #model = load('bin.3')
 
-    #x_test = np.array([0,5,0,6,0,7,1,6]).reshape(4,2)
-    x_test = np.array([3,5,6,7]).reshape(4,1)
+    #x_test = np.array([0,5,0,6,0,7,0,9,1,2,1,3,1,4,1,5]).reshape(6,2)
+    #x_test = np.array([0,1,2,3,4,5,6,7]).reshape(8,1)
+    #x_test = np.array([0,1,2,3]).reshape(4,1)
+    #x_test = X_train[1:2**binlen]
     y_seq = model.predict(x_test)
+    print(x_test)
     print(y_seq)
+    print(y_test)
     print(y_seq==1.0)
-    print(bin(13))
     dump(model,name)
     
 
