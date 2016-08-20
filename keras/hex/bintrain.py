86cbfec9c4ca5dd6ef43ad7c9b5d2104e54cf555
alingse <alingse@foxmail.com>
2016-08-20 11:47:15 +0800

    update:clean:调整优化代码

--- keras/hex/bintrain.py
+++ keras/hex/bintrain.py
@@ -7,10 +7,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Activation
-from keras.models import model_from_json
 
 from keras.optimizers import SGD, Adadelta, Adagrad,RMSprop
 from keras.utils import np_utils, generic_utils
+
+from utils import activations
+from utils import dump,load
+
 
 import numpy as np
 from random import shuffle
@@ -18,10 +21,8 @@ from random import choice
 
 import sys
 
-
-#0 --> 255
-#0b0 --> 0b11111111
-#seq
+#binlen = 8 :0b1111 --> [0,0,0,0,1,1,1,1]
+#numbeln = 3: 15 --> [0,1,5]
 def load_XY(binlen = 8):
     maxnum = eval('0b'+'1'*binlen)
     numlen = len(str(maxnum))
@@ -54,38 +55,21 @@ def load_XY(binlen = 8):
     return X_train,Y_train,x_test,y_test
 
 
-def train(X_train,Y_train):
-
+def train(X_train,Y_train,actives):
     numlen = X_train.shape[1]
     binlen = Y_train.shape[1]
     print(numlen,binlen)
-
-    #copy from keras
-    model = Sequential()
-
-    activations = ['softmax','softplus','softsign','relu','tanh','sigmoid','hard_sigmoid','linear']
-    actives = [choice(activations) for i in range(3)]
-
-    #['softplus', 'softmax', 'tanh']
-    #['relu', 'sigmoid', 'linear']
-    #['relu', 'sigmoid', 'hard_sigmoid']
-    #['sigmoid', 'softmax', 'tanh']
-    #['linear', 'softsign', 'tanh']
-    #['tanh', 'sigmoid', 'softsign']
-    #['hard_sigmoid', 'tanh', 'softsign']
-    #['softplus', 'tanh', 'softsign']
-    #['softplus', 'sigmoid', 'softplus']
-    #actives = ['relu', 'tanh', 'tanh']
-    actives = ['tanh', 'sigmoid', 'softsign']
     print(actives)
+
+
+    model = Sequential()
 
     model.add(Dense(2*binlen,input_dim=numlen,activation=actives[0],init='uniform'))
     model.add(Dense(4*binlen,input_dim=2*binlen,activation=actives[1],init='normal'))
     model.add(Dense(2*binlen,input_dim=4*binlen,activation=actives[2]))
     model.add(Dense(binlen,input_dim=2*binlen,activation='hard_sigmoid'))
 
-
-    rmsprop = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
+    rmsprop = RMSprop(lr=0.005, rho=0.9, epsilon=1e-08)
 
     model.compile(loss='binary_crossentropy',
                   optimizer=rmsprop,
@@ -93,7 +77,7 @@ def train(X_train,Y_train):
 
     print('start fit')
     model.fit(X_train, Y_train,
-              batch_size=600, nb_epoch=250*binlen,
+              batch_size=600, nb_epoch=max(1000,250*binlen),
               verbose=2,shuffle=True,
               validation_split=0.2)
 
@@ -101,40 +85,58 @@ def train(X_train,Y_train):
     return model
 
 
-def dump(model,save_name):
-    with open('{}.model.json'.format(save_name),'w') as f:
-        f.write(model.to_json())
-    model.save_weights('{}.model.weigthts.h5'.format(save_name))
-
-
-def load(name):
-    with open('{}.model.json'.format(name),'r') as f:
-        model = model_from_json(f.read())
-    model.load_weights('{}.model.weigthts.h5'.format(name))
-    return model
-
-
-def main(binlen=4,name='test'):
+def main(binlen=4,name='test',need_train=True,test_active=True):
+    print(need_train,test_active)
 
     X_train,Y_train,x_test,y_test = load_XY(binlen=binlen)
-    
-    model = train(X_train,Y_train)
-    score = model.evaluate(X_train,Y_train,batch_size=20,verbose=2)
-    print(score)
-    #model = load('bin.3')
 
-    #x_test = np.array([0,5,0,6,0,7,0,9,1,2,1,3,1,4,1,5]).reshape(6,2)
-    #x_test = np.array([0,1,2,3,4,5,6,7]).reshape(8,1)
-    #x_test = np.array([0,1,2,3]).reshape(4,1)
-    #x_test = X_train[1:2**binlen]
+    if need_train:
+        if test_active:
+            actives = [choice(activations) for i in range(3)]
+        else:
+            #['relu', 'sigmoid', 'linear']
+            #['relu', 'sigmoid', 'hard_sigmoid']
+            #['sigmoid', 'softmax', 'tanh']
+            #['sigmoid', 'tanh', 'softplus']
+            #['linear', 'softsign', 'tanh']
+            #['linear', 'sigmoid', 'softsign']
+            #['linear', 'softmax', 'tanh']
+            #['linear', 'hard_sigmoid', 'softsign']
+            #['hard_sigmoid', 'tanh', 'softsign']        
+            #['softplus', 'sigmoid', 'softplus']
+            #['softplus', 'softmax', 'tanh']
+            #['softplus', 'softsign', 'relu']
+            #['softsign', 'softmax', 'softplus']
+            #['tanh', 'relu', 'relu']
+            #['tanh', 'softmax', 'softmax']
 
+            #['softplus', 'tanh', 'softsign']
+            #['relu', 'softmax', 'tanh']
+            #['relu', 'tanh', 'softsign']
+            #['linear', 'tanh', 'linear']
+            #['tanh', 'sigmoid', 'softsign']
+            actives = ['tanh', 'sigmoid', 'softsign']
+            actives = ['linear', 'tanh', 'linear']
+            actives = ['relu', 'tanh', 'softsign']
+            actives = ['relu', 'softmax', 'tanh']
+            actives = ['softplus', 'tanh', 'softsign']
+
+        #for train
+        model = train(X_train,Y_train,actives)
+        #score
+        score = model.evaluate(X_train,Y_train,batch_size=20,verbose=2)
+        print(score)
+    else:
+        model = load(name)
 
     y_seq = model.predict(x_test)
     y_seq2 = np.float32(y_seq>0.5)
 
-    if np.sum(np.abs(y_seq - y_test)) == 0.0:
-        print(True)
-    #exit()
+    if test_active:
+        if np.sum(np.abs(y_seq - y_test)) == 0.0:
+            print(True)
+        #not test
+        exit()
 
     print(x_test)
     print(y_seq)
@@ -144,11 +146,18 @@ def main(binlen=4,name='test'):
     print(y_seq2==y_test)
     print('diff:sum: |y_seq - y_test|',np.sum(np.abs(y_seq - y_test)))
     print('diff:sum: |y_seq2 - y_test|',np.sum(np.abs(y_seq2 - y_test)))
-    dump(model,name)
+
+    if need_train:
+        dump(model,name)
 
 if __name__ == '__main__':
-    name = 'bin'
-    binlen = 4
-    if len(sys.argv) == 2:
-        binlen = int(sys.argv[1])
-    main(binlen,name)
+    need_train = True
+    test_active = True
+    #test_active = False
+
+    #binlen = 4
+    binlen = int(sys.argv[1])
+    
+    name = 'bin.test.'+str(binlen)
+    
+    main(binlen,name=name,need_train=need_train,test_active=test_active)
